78616bff6dc010d77321bbc0c0c16399d5cbfabc
Deftu <deftudev@gmail.com>
2024-02-18 23:29:45 +0200

    Remove encryption key constant

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
 
-export const ENCRYPTION_KEY = Buffer.from(process.env.ENCRYPTION_KEY, "hex");
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const port = process.env.PORT || 3000;
